4a6800771d7c10c7c78e282f8f8ebb376f22b0f6
Aniekan Essien <36553605+nakessien@users.noreply.github.com>
2020-08-18 01:17:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from distutils.core import setup
 
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 
 setup(
   name = 'tl4sm',         # How you named your package folder (MyLib)
